f20aa1cd032b18d2714acfbce64228a9732d019b
sudrain <densedra@gmail.com>
2025-04-23 13:13:52 +0500

    first light version

---
+++ app.js
@@ -0,0 +1,54 @@
+let scetchColor = 'red'
+
+function drawField(size = 32) {
+    let field = document.querySelector('#container');
+    for (i = 0; i < size; i++) {
+        let row = document.createElement('div');
+        row.setAttribute("class", "row");
+        field.appendChild(row);
+        for (x = 0; x < size; x++) {
+            let col = document.createElement('div');
+            col.setAttribute("class", "col");
+            row.appendChild(col);
+        }
+    }
+    catchMouseMove();
+}
+
+function removeField() {
+    let field = document.querySelectorAll('.row');
+    field.forEach((e) => { e.remove() })
+}
+
+function catchMouseMove() {
+    let field = document.querySelectorAll('.col');
+    field.forEach(
+        function (node) {
+            node.addEventListener('mouseover', (node) => { node.target.style.background = scetchColor; });
+        }
+    );
+}
+
+function changeSize() {
+    button = document.querySelector('#size');
+    button.addEventListener('click', function () {
+        size = prompt('Set field 16 - 100', 32);
+        if (!(15 < size < 101)) {
+            return alert('Only 16 - 100 numbers')
+        }
+        removeField();
+        drawField(size);
+    })
+}
+
+function clearField() {
+    button = document.querySelector('#clear');
+    button.addEventListener('click', function () {
+        removeField();
+        drawField();
+    })
+}
+
+drawField();
+changeSize();
+clearField();
